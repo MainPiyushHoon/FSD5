cab097871ec2f9754fc40fbf342b3cd581104e3d
Piyush <Piyushverma2005.pv2@gmail.com>
2026-09-08 09:11:25 +0530

    async await

--- Javascript/async_await.js
+++ Javascript/async_await.js
@@ -16,8 +16,36 @@ function f2() {
     })
 }
 
-f1()
-    .then(f2)
-    .catch((err) => {
-        console.log("ERROR", err);
+function f3() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("How are you?");
+            resolve();
+        }, 2000);
     })
+}
+
+function f4() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("I am fine");
+            resolve();
+        }, 3000);
+    })
+}
+
+
+async function test() {
+    try{
+
+        await f1();
+        await f2();
+        await f3();
+        await f4();
+    } 
+    catch(err) {
+        console.log("ERROR", err);
+    }
+}
+
+test();
